ff403a49ab16e5bb5b18c2f9318b896cff59ccb3
Bohan Leng <lamberhand@gmail.com>
2025-10-18 17:04:38 +0200

    Defined the graph model exchange file format and parsing method, including random generator

---
+++ labelled_graph.py
@@ -0,0 +1,67 @@
+import json
+from typing import Callable
+from random_util import RandomFactory
+
+GRAPH_MODEL_EXAMPLE_FILE = "graph_model_example.json"
+
+class Vertex:
+    def __init__(self, vertex_json: dict):
+        self.id: int = vertex_json["id"]
+        self.name: str = vertex_json["name"]
+        self.buffer_capacity: int = vertex_json["buffer_capacity"]
+        self.service_distribution_type: str = vertex_json["service_time_distribution"]["type"]
+        self.service_distribution_params: list[float] = vertex_json["service_time_distribution"]["parameters"]
+        self.service: Callable[[], float] \
+            = RandomFactory.get_random_generator_from_json(vertex_json["service_time_distribution"])
+
+    def __str__(self):
+        return ((f"Vertex {self.id}: name={self.name}, buffer_capacity={self.buffer_capacity}, \n"
+                f"service_time_distribution={self.service_distribution_type}{self.service_distribution_params}, \n"
+                f"sample_service_time={self.service()} \n"))
+
+class Arc:
+    def __init__(self, arc_json: dict):
+        self.tail: int = arc_json["tail"]
+        self.head: int = arc_json["head"]
+        self.transfer_time_distribution_type: str = arc_json["transfer_time_distribution"]["type"]
+        self.transfer_time_distribution_params: list[float] = arc_json["transfer_time_distribution"]["parameters"]
+        self.transfer: Callable[[], float] \
+            = RandomFactory.get_random_generator_from_json(arc_json["transfer_time_distribution"])
+
+    def __str__(self):
+        return ((f"Arc from {self.tail} to {self.head}, \n"
+                f"transfer_time_distribution={self.transfer_time_distribution_type}"
+                f"{self.transfer_time_distribution_params}, \n"
+                f"sample_transfer_time={self.transfer()} \n"))
+
+
+
+class LabelledGraph:
+    def __init__(self, name: str, graph_json: dict):
+        self.name: str = name
+        self.vertices: dict[int, Vertex] = {}
+        self.arcs: list[Arc] = []
+        for vertex_json in graph_json["vertices"]:
+            vertex = Vertex(vertex_json)
+            self.vertices[vertex.id] = vertex
+        for arc_json in graph_json["arcs"]:
+            arc = Arc(arc_json)
+            self.arcs.append(arc)
+
+    def __str__(self):
+        vertices_str = "\n".join(str(v) for v in self.vertices.values())
+        arcs_str = "\n".join(str(a) for a in self.arcs)
+        return (f"{'=' * 50}\n"
+                f"Labelled graph \"{self.name}\" has {len(self.vertices)} vertices and {len(self.arcs)} arcs.\n\n"
+                f"{vertices_str}\n{arcs_str}\n"
+                f"{'=' * 50}")
+
+
+if __name__ == "__main__":
+    with open(GRAPH_MODEL_EXAMPLE_FILE, "r") as f:
+        graph_json = json.load(f)
+
+    graph = LabelledGraph("Example Graph", graph_json)
+    print(graph)
+
+
---
+++ random_util.py
@@ -0,0 +1,53 @@
+import numpy as np
+from typing import Callable
+import time
+
+
+class RandomFactory:
+    @staticmethod
+    def get_random_generator_from_json(distribution_json: dict, seed: int = None) -> Callable[[], float]:
+        seed_used = seed if seed is not None else time.time_ns()
+        rng = np.random.default_rng(seed_used)
+
+        if distribution_json["type"] == "uniform":
+            if len(distribution_json["parameters"]) != 2:
+                raise ValueError("Uniform distribution requires exactly 2 parameters: low and high")
+            low = distribution_json["parameters"][0]
+            high = distribution_json["parameters"][1]
+            return lambda: rng.uniform(low, high)
+
+        if distribution_json["type"] == "normal":
+            if len(distribution_json["parameters"]) != 2:
+                raise ValueError("Normal distribution requires exactly 2 parameters: mean and stddev")
+            mean = distribution_json["parameters"][0]
+            stddev = distribution_json["parameters"][1]
+            return lambda: rng.normal(mean, stddev)
+
+        if distribution_json["type"] == "constant":
+            if len(distribution_json["parameters"]) != 1:
+                raise ValueError("Constant distribution requires exactly 1 parameter: value")
+            value = distribution_json["parameters"][0]
+            return lambda: value
+
+        if distribution_json["type"] == "exponential":
+            if len(distribution_json["parameters"]) != 1:
+                raise ValueError("Exponential distribution requires exactly 1 parameter: mean")
+            mean = distribution_json["parameters"][0]
+            return lambda: rng.exponential(mean)
+
+        if distribution_json["type"] == "triangular":
+            if len(distribution_json["parameters"]) != 3:
+                raise ValueError("Triangular distribution requires exactly 3 parameters: left, right, mode")
+            left = distribution_json["parameters"][0]
+            right = distribution_json["parameters"][1]
+            mode = distribution_json["parameters"][2]
+            return lambda: rng.triangular(left, mode, right)
+
+        if distribution_json["type"] == "weibull":
+            if len(distribution_json["parameters"]) != 2:
+                raise ValueError("Weibull distribution requires exactly 2 parameter: shape, scale")
+            shape = distribution_json["parameters"][0]
+            scale = distribution_json["parameters"][1]
+            return lambda: rng.weibull(shape) * scale
+
+        return lambda: 0.0
